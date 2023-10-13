202a118986dd7bb3bbe7a267e04a86a7dbe843e8
Agustín Ruiz <143494692+GoldenDrk@users.noreply.github.com>
2023-10-13 22:33:54 +0000

    Últimos ajustes

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,21 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let cover2 = `<div class="cover2"><img src="${variables.avatarURL}" class="photo" /></div>`;
+  if (variables.includeProfilePicture == false)
+    cover2 = `<div class="cover2"></div>`;
 
-  // reset the website body with the new html output
+  // reset the website body with the new html output <img src="${variables.avatarURL}" class="photo" />
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
+            ${cover2}
           <h1>${variables.name} ${variables.lastName}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
-          <ul class="${variables.socialMediaPosition}">
+          <ul class="${variables.socialMediaPosition} id="socialMedia">
             <li><a href="https://twitter.com/${variables.twitter}" target="_blank"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}" target="_blank"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/${variables.linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${variables.instagram}" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
@@ -54,6 +57,7 @@ window.onload = function() {
   window.variables = {
     // if includeCover is true the algorithm should show the cover image
     includeCover: false,
+    includeProfilePicture: false,
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
@@ -65,11 +69,11 @@ window.onload = function() {
     github: null,
     linkedin: null,
     instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    name: "Name",
+    lastName: "Last Name",
+    role: "Role",
+    country: "Country",
+    city: "City"
   };
   render(window.variables); // render the card for the first time
 
